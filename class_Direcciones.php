d560485425678e38d4ce90ec0b28886d10f2c8c5
lquiroga <lquiroga@usal.edu.ar>
2019-08-02 15:59:02 -0300

    nuevas clases

--- class_Direcciones.php
+++ class_Direcciones.php
@@ -10,7 +10,7 @@
  * @name class_direccion.php
  * @version 0.1 version inicial del archivo.
  */
-use function Direcciones\cargar_db_apers;
+//use function Direcciones\cargar_db_apers;
 
 require_once 'class_conexion.php';
 
