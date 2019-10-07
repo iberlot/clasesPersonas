94c0db416f99fb4c578781eff4a9b74952c64dbe
lquiroga <lquiroga@usal.edu.ar>
2019-10-07 18:08:36 -0300

    INSERTAR DATA EXRTA MERCADOPAGO . Uso de clase class_TransaccionesMercadopago. Creacion de clase en clasesusal e  implementacion de la misma en solitram

---
+++ class_TransaccionesMercadopago.php
@@ -0,0 +1,316 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once ("class_transacciones.php");
+
+/**
+ * Description of class_TransaccionesMercadopago
+ * 
+ * Carga datos de las transacciones realizadas con mercadopago en la tabla DATATRANSACCIONESMERCADOPAGO
+ *
+ * @property tabla donde trabaja DATATRANSACCIONESMERCADOPAGO 
+ * Description:
+ * ID
+ * IDFORM
+ * COLLECTOR_ID
+ * DATECREATED
+ * DATEAPPROVED
+ * OPERATIONTYPE
+ * PAYMENTMETHODID
+ * ORDERID
+ * ORDERTYPE
+ * PAYERNAME
+ * PAYEREMAIL
+ * FEEMP
+ * FEETYPE
+ * TRANSACTIONAMOUNT
+ * NET_RECEIVED_AMOUNT
+ * CUOTAS_INSTALLMENT_AMOUNT
+ * TOTAL_PAID_AMOUNT
+ * 
+ * 
+ * @author lquiroga
+ */
+class TransaccionesMercadopago extends Transacciones {
+
+    protected $db;
+    protected $id;
+    protected $idform;
+    protected $collector_id;
+    protected $datecreated;
+    protected $dateapproved;
+    protected $operationtype;
+    protected $paymentmethodid;
+    protected $orderid;
+    protected $ordertype;
+    protected $payername;
+    protected $payeremail;
+    protected $feemp;
+    protected $feetype;
+    protected $transactionamount;
+    protected $net_received_amount;
+    protected $cuotas_installment_amount;
+    protected $total_paid_amount;
+
+    public function __construct($db = null, $idform = null) {
+
+        parent::__construct($db,$idform);
+
+        if ($idform != null && trim($idform) != '') {
+
+            $query = "SELECT * FROM TESORERIA.DATATRANSACCIONESMERCADOPAGO WHERE idform = :idform";
+
+            $parametros = array();
+
+            $parametros['idform'] = $idform;
+
+            $result = $this->db->query($query, true, $parametros);
+
+            $this->loadData($this->db->fetch_array($result));
+        }
+    }
+
+    
+    /**
+    * Salva datos en la tabla transacciones
+    * insertTransac
+    *
+    * tabla :
+    * ID- IDFORM   COLLECTOR_ID- DATECREATED- DATEAPPROVED
+    * OPERATIONTYPE    PAYMENTMETHODID    ORDERID    ORDERTYPE    PAYERNAME    PAYEREMAIL
+    * FEEMP    FEETYPE    TRANSACTIONAMOUNT    NET_RECEIVED_AMOUNT    CUOTAS_INSTALLMENT_AMOUNT    TOTAL_PAID_AMOUNT
+    *
+    * @param array $datos
+    * @return bool
+    *
+    */
+    public function insertTransacMercado($datos){
+
+            // $db = Conexion::openConnection();
+
+            $datos['ID'] = 'tesoreria.TRANSACCIONESMERCADOPAGO_SEQ.nextval';
+
+            $insercion = $this->db->realizarInsert ($datos, 'tesoreria.DATATRANSACCIONESMERCADOPAGO');
+
+            return $insercion;
+    }
+    /**
+     * loadData
+     * Carga propiedades del objeta que vienen desde la DB
+     *
+     * @param array $fila
+     *        	return objet TRANSACCIONMERCADOPAGO
+     */
+    public function loadDataMercado($fila) {
+
+
+        if (isset($fila['id'])) {
+            $this->setId($fila['ID']);
+        }
+
+        if (isset($fila['collector_id'])) {
+            $this->setCollector_id($fila['collector_id']);
+        }
+
+        if (isset($fila['datecreated'])) {
+                    $this->setDatecreated($fila['datecreated']);
+        }
+
+        if (isset($fila['dateapproved'])) {
+                    $this->setDateapproved($fila['dateapproved']);
+        }
+
+        if (isset($fila['operationtype'])) {
+                    $this->setOperationtype($fila['operationtype']);
+        }
+
+        if (isset($fila['paymentmethodid'])) {
+                    $this->setPaymentmethodid($fila['paymentmethodid']);
+        }
+
+        if (isset($fila['orderid'])) {
+                    $this->setOrderid($fila['orderid']);
+        }
+
+        if (isset($fila['ordertype'])) {
+                    $this->setOrdertype($fila['ordertype']);
+        }
+
+        if (isset($fila['payername'])) {
+                    $this->setPayername($fila['payername']);
+        }
+
+        if (isset($fila['payeremail'])) {
+                    $this->setPayeremail($fila['payeremail']);
+        }
+
+        if (isset($fila['feemp'])) {
+                    $this->setFeemp($fila['feemp']);
+        }
+
+        if (isset($fila['feetype'])) {
+                    $this->setFeetype($fila['feetype']);
+        }
+
+        if (isset($fila['transactionamount'])) {
+                    $this->setTransactionamount($fila['transactionamount']);
+        }
+
+        if (isset($fila['net_received_amount'])) {
+                    $this->setNet_received_amount($fila['net_received_amount']);
+        }
+
+        if (isset($fila['cuotas_installment_amount'])) {
+                    $this->setCuotas_installment_amount($fila['cuotas_installment_amount']);
+        }
+
+        if (isset($fila['total_paid_amount'])) {
+                    $this->setTotal_paid_amount($fila['total_paid_amount']);
+        }
+    }
+
+    /*     * ******GETTERS*********** */
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getIdform() {
+        return $this->idform;
+    }
+
+    function getCollector_id() {
+        return $this->collector_id;
+    }
+
+    function getDatecreated() {
+        return $this->datecreated;
+    }
+
+    function getDateapproved() {
+        return $this->dateapproved;
+    }
+
+    function getOperationtype() {
+        return $this->operationtype;
+    }
+
+    function getPaymentmethodid() {
+        return $this->paymentmethodid;
+    }
+
+    function getOrderid() {
+        return $this->orderid;
+    }
+
+    function getOrdertype() {
+        return $this->ordertype;
+    }
+
+    function getPayername() {
+        return $this->payername;
+    }
+
+    function getPayeremail() {
+        return $this->payeremail;
+    }
+
+    function getFeemp() {
+        return $this->feemp;
+    }
+
+    function getFeetype() {
+        return $this->feetype;
+    }
+
+    function getTransactionamount() {
+        return $this->transactionamount;
+    }
+
+    function getNet_received_amount() {
+        return $this->net_received_amount;
+    }
+
+    function getCuotas_installment_amount() {
+        return $this->cuotas_installment_amount;
+    }
+
+    function getTotal_paid_amount() {
+        return $this->total_paid_amount;
+    }
+
+    /*     * ******SETTERS*********** */
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setIdform($idform) {
+        $this->idform = $idform;
+    }
+
+    function setCollector_id($collector_id) {
+        $this->collector_id = $collector_id;
+    }
+
+    function setDatecreated($datecreated) {
+        $this->datecreated = $datecreated;
+    }
+
+    function setDateapproved($dateapproved) {
+        $this->dateapproved = $dateapproved;
+    }
+
+    function setOperationtype($operationtype) {
+        $this->operationtype = $operationtype;
+    }
+
+    function setPaymentmethodid($paymentmethodid) {
+        $this->paymentmethodid = $paymentmethodid;
+    }
+
+    function setOrderid($orderid) {
+        $this->orderid = $orderid;
+    }
+
+    function setOrdertype($ordertype) {
+        $this->ordertype = $ordertype;
+    }
+
+    function setPayername($payername) {
+        $this->payername = $payername;
+    }
+
+    function setPayeremail($payeremail) {
+        $this->payeremail = $payeremail;
+    }
+
+    function setFeemp($feemp) {
+        $this->feemp = $feemp;
+    }
+
+    function setFeetype($feetype) {
+        $this->feetype = $feetype;
+    }
+
+    function setTransactionamount($transactionamount) {
+        $this->transactionamount = $transactionamount;
+    }
+
+    function setNet_received_amount($net_received_amount) {
+        $this->net_received_amount = $net_received_amount;
+    }
+
+    function setCuotas_installment_amount($cuotas_installment_amount) {
+        $this->cuotas_installment_amount = $cuotas_installment_amount;
+    }
+
+    function setTotal_paid_amount($total_paid_amount) {
+        $this->total_paid_amount = $total_paid_amount;
+    }
+
+}
---
+++ class_transacciones.php
@@ -0,0 +1,359 @@
+<?php
+
+require_once ("/web/html/classesUSAL/class_Personas.php");
+require_once ("/web/html/classesUSAL/class_derechos_varios.php");
+require_once ("/web/html/classesUSAL/class_alumnos.php");
+require_once ("/web/html/classesUSAL/class_carreras.php");
+require_once ("/web/html/classesUSAL/class_FormsSolitram.php");
+
+class Transacciones {    
+
+   protected $idtransaccion;
+   protected $idformulario;
+   protected $canal;
+   protected $idcentrodecosto;
+   protected $monto;
+   protected $estadotransaccion;
+   protected $fechatransaccion;
+   protected $fechaproceso;
+   protected $estadoproceso;
+   protected $nro_transaccion;       
+    
+    public function __construct($db, $idtransaccion=null ) {
+                
+        $this->setDb($db);
+        
+        if($idtransaccion != null){            
+            
+            $parametros = array (
+                $idtransaccion
+            );
+
+            $query  = "select * from TESORERIA.TRANSACCIONES WHERE IDTRANSACCION = :idtransac";
+
+            $result = $this->db->query ($query, true, $parametros);
+
+            if ($result){
+
+                    $arr_asoc = $db->fetch_array ($result);
+
+                    $this->loadData ($arr_asoc);
+            }
+            
+        }
+        
+    }
+    
+    /**
+     * Salva datos en la tabla transacciones
+     * insertTransac
+     *
+     * tabla :
+     * IDTRANSACCION-IDFORMULARIO-CANAL-IDCENTRODECOSTO-MONTO-ESTADOTRANSACCION
+     * FECHATRANSACCION-FECHAPROCESO-ESTADOPROCESO-NRO_TRANSACCION
+     *
+     * @param array $datos
+     * @return bool
+     *
+     */
+    public function insertTransac($datos){
+
+            // $db = Conexion::openConnection();
+
+            $datos['IDTRANSACCION'] = 'tesoreria.idtransaccion_seq.nextval';
+
+            $insercion = $this->db->realizarInsert ($datos, 'tesoreria.TRANSACCIONES');
+
+            return $insercion;
+    }
+        
+    /**
+    * Hace un inserta de los campos que pasemos y la tabla
+    *
+    * @param
+    *        	array data_update -->El array de datos se maneja con indices y valores ej:$datos['PERSON'] ='alumno'
+    * @param
+    *        	strin tabla -->tabla a actualizar
+    * @param
+    *        	string where -->Condicion para actualizar
+    *
+    * @return bool
+    */
+	public function updateTransaccion($data_update, $tabla, $where ){
+            
+            // $db = Conexion::openConnection();
+            $insercion = $this->db->realizarUpdate ($data_update, $tabla, $where);
+
+            /* SI SE REALIZO LA CORRECTAMENTE UPDATE DE HISTORIAL */
+            if ($insercion){
+               echo('OK'); 
+            }
+        }
+        
+        
+    
+    /**
+    *
+    * loadData
+    * Carga propiedades del objeta que vienen desde la DB
+    *
+    * @param array $fila
+    *        	return objet form
+    *
+    */
+	public function loadData($fila)
+	{
+  
+		if (isset ($fila['idtransaccion'])){
+                    
+			$this->setIdtransaccion($fila['idtransaccion']);
+		}
+
+		if (isset ($fila['IDFORMULARIO'])){
+                    
+			$this->setIdformulario($fila['IDFORMULARIO']);
+		}
+
+
+		if (isset ($fila['CANAL'])){
+                    
+			$this->setCanal($fila['CANAL']);
+		}
+
+		if (isset ($fila['IDCENTRODECOSTO'])){
+                    
+			$this->setIdcentrodecosto($fila['IDCENTRODECOSTO']);
+		}
+
+		if (isset ($fila['MONTO'])){
+                    
+			$this->setMonto($fila['MONTO']);
+		}
+
+		if (isset ($fila['ESTADOTRANSACCION'])){
+                    
+			$this->setEstadotransaccion($fila['ESTADOTRANSACCION']);
+		}
+
+		if (isset ($fila['FECHATRANSACCION'])){
+                    
+			$this->setFechatransaccion($fila['FECHATRANSACCION']);
+		}
+
+		if (isset ($fila['ESTADOPROCESO'])){
+                    
+			$this->setEstadoproceso($fila['ESTADOPROCESO']);
+		}
+
+		if (isset ($fila['NRO_TRANSACCION'])){
+                    
+			$this->setNro_transaccion ($fila['NRO_TRANSACCION']);
+		}
+
+	}
+        
+        
+        
+    /****GETTERS******/
+    function getDb(){
+        return $this->db;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable $idtransaccion
+     */
+    function getIdtransaccion(){
+        return $this->idtransaccion;
+    }
+    
+    /**
+     *
+     * @return string el dato de la variable idformulario
+    */
+    function getIdformulario(){
+        return $this->idformulario;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable canal
+     */
+    function getCanal(){
+        return $this->canal;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable idcentrodecosto
+     */
+    function getIdcentrodecosto(){
+        return $this->idcentrodecosto;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable monto
+     */
+    public function getMonto(){
+        return $this->monto;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable estadotransaccion
+     */
+    function getEstadotransaccion(){
+        return $this->estadotransaccion;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable fechatransaccion
+     */
+    public function getFechatransaccion(){
+        return $this->fechatransaccion;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable fechaproceso
+     */
+    function getFechaproceso() {
+        return $this->fechaproceso;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable estadoproceso
+     */
+    function getEstadoproceso() {
+        return $this->estadoproceso;
+    }
+
+    /**
+     *
+     * @return string el dato de la variable nro_transaccion
+     */
+    function getNro_transaccion() {
+        return $this->nro_transaccion;
+    }
+    
+    /****SETTERS******/
+
+    function setDb($db) {
+        $this->db = $db;
+    }
+
+    /**
+    * Setter del parametro $idtransaccion de la clase.
+    *
+    * @param number $idtransaccion
+    *        	dato a cargar en la variable.
+    */
+    function setIdtransaccion($idtransaccion) {
+        $this->idtransaccion = $idtransaccion;
+    }
+
+        
+    /**
+    * Setter del parametro $idformulario de la clase.
+    *
+    * @param number $idformulario
+    *        	dato a cargar en la variable.
+    */
+    function setIdformulario($idformulario) {
+        $this->idformulario = $idformulario;
+    }
+    
+
+    /**
+    * Setter del parametro $canal de la clase.
+    *
+    * @param string $canal
+    *        	dato a cargar en la variable.
+    */
+    function setCanal($canal) {
+        $this->canal = $canal;
+    }
+
+    /**
+    * Setter del parametro $idcentrodecosto de la clase.
+    *
+    * @param number $idcentrodecosto
+    *        	dato a cargar en la variable.
+    */
+    function setIdcentrodecosto($idcentrodecosto) {
+        $this->idcentrodecosto = $idcentrodecosto;
+    }
+
+        
+    /**
+    * Setter del parametro $monto de la clase.
+    *
+    * @param number $monto
+    *        	dato a cargar en la variable.
+    */
+    function setMonto($monto) {
+        $this->monto = $monto;
+    }
+
+    
+    /**
+    * Setter del parametro $estadotransaccion de la clase.
+    *
+    * @param string $estadotransaccion
+    *        	dato a cargar en la variable.
+    */
+    function setEstadotransaccion($estadotransaccion) {
+        $this->estadotransaccion = $estadotransaccion;
+    }
+
+    
+    /**
+    * Setter del parametro $fechatransaccion de la clase.
+    *
+    * @param string $fechatransaccion
+    *        	dato a cargar en la variable.
+    */
+    function setFechatransaccion($fechatransaccion) {
+        $this->fechatransaccion = $fechatransaccion;
+    }
+
+        /**
+    * Setter del parametro $fechaproceso de la clase.
+    *
+    * @param string $fechaproceso
+    *        	dato a cargar en la variable.
+    */
+    function setFechaproceso($fechaproceso) {
+        $this->fechaproceso = $fechaproceso;
+    }
+
+        /**
+    * Setter del parametro $estadoproceso de la clase.
+    *
+    * @param number $estadoproceso
+    *        	dato a cargar en la variable.
+    */
+    function setEstadoproceso($estadoproceso) {
+        $this->estadoproceso = $estadoproceso;
+    }
+
+        /**
+    * Setter del parametro $nro_transaccion de la clase.
+    *
+    * @param number $nro_transaccion
+    *        	dato a cargar en la variable.
+    */
+    function setNro_transaccion($nro_transaccion) {
+        $this->nro_transaccion = $nro_transaccion;
+    }
+
+               
+        
+        
+        
+}
+
+?>
